d9fc2645209baaba9e5350a0b7e7c6d3d3b4c0e9
SthephanySouza <101808324+SthephanySouza@users.noreply.github.com>
2023-05-13 10:22:14 -0300

    Update
    Adicionando slideshow

--- Fast-Fitness/script.js
+++ Fast-Fitness/script.js
@@ -1,16 +1,25 @@
-function sizeOfThings(){
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
+let Slide = 0;
+SlideShow();
+
+function SlideShow() {
+    let i;
+    let slides = document.getElementsByClassName("imgSlide");
+    let pontos = document.getElementsByClassName("ponto");
     
-    var screenWidth = screen.width;
-    var screenHeight = screen.height;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    Slide++;
     
-    document.querySelector('.window-size').innerHTML = windowWidth + 'x' + windowHeight;
-    document.querySelector('.screen-size').innerHTML = screenWidth + 'x' + screenHeight;
+    if (Slide > slides.length){
+        Slide = 1
+    }    
   
-  };
-  sizeOfThings();
-  
-  window.addEventListener('resize', function(){
-      sizeOfThings();
-  });
+    for (i = 0; i < pontos.length; i++) {
+        pontos[i].className = pontos[i].className.replace(" ativar", "");
+    }
+
+    slides[Slide-1].style.display = "block";  
+    pontos[Slide-1].className += " ativar";
+    setTimeout(SlideShow, 3000);
+}
