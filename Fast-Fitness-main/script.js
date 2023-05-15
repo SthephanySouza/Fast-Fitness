9208e71f5be748ba4ff664aef60a4fa39d92fb63
SthephanySouza <101808324+SthephanySouza@users.noreply.github.com>
2023-05-15 18:49:54 -0300

    Update
    Tentando adicionar menu hamburguer

--- Fast-Fitness-main/script.js
+++ Fast-Fitness-main/script.js
@@ -1,29 +1,3 @@
-let Slide = 0;
-SlideShow();
-
-function SlideShow() {
-    let i;
-    let slides = document.getElementsByClassName("imgSlide");
-    let pontos = document.getElementsByClassName("ponto");
-    
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-    }
-    Slide++;
-    
-    if (Slide > slides.length){
-        Slide = 1
-    }    
-  
-    for (i = 0; i < pontos.length; i++) {
-        pontos[i].className = pontos[i].className.replace(" ativar", "");
-    }
-
-    slides[Slide-1].style.display = "block";  
-    pontos[Slide-1].className += " ativar";
-    setTimeout(SlideShow, 3000);
-}
-
 let $calcular = document.getElementById("calcular");
 
 function imc(){
